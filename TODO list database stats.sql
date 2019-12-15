5f82e0124010d8deaa9356dc89370332e3236174
Konstrictor <jonkur96@gmail.com>
2019-12-15 15:44:05 +0200

    TODO list database stats 1

--- TODO list database stats.sql
+++ TODO list database stats.sql
@@ -2,3 +2,4 @@ CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
 INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
 INSERT INTO todo_list VALUES (2, "vacuuming", 20);
 INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+INSERT INTO todo_list VALUES (4, "Learn some stuff on BujaHUB", 60);
