fa952861432a2df0973cd3a2d046ed20087d1547
Konstrictor <jonkur96@gmail.com>
2019-12-15 15:35:50 +0200

    Box office hits database 2

--- Box office hits database.sql
+++ Box office hits database.sql
@@ -5,5 +5,6 @@ INSERT INTO movies VALUES (3, "Star Wars: Episode IV - A New Hope", 1977);
 INSERT INTO movies VALUES (4, "Shrek 2", 2004);
 INSERT INTO movies VALUES (5, "The Lion King", 1994);
 INSERT INTO movies VALUES (6, "Disney's Up", 2009);
-
+ 
 SELECT * FROM movies;
+SELECT * FROM movies WHERE release_year >= 2000 ORDER BY release_year ASC;
