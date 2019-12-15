997997f59d9bba95b3195276a53f1e1aca282d3e
Konstrictor <jonkur96@gmail.com>
2019-12-15 15:42:11 +0200

    TODO list database stats

---
+++ TODO list database stats.sql
@@ -0,0 +1,4 @@
+CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
+INSERT INTO todo_list VALUES (2, "vacuuming", 20);
+INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
