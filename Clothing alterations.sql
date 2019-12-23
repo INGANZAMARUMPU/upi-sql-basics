0db6a5c53a83e6c0b1df0df5cd3a396de74affd8
Konstrictor <jonkur96@gmail.com>
2019-12-23 11:30:18 +0200

    Clothing alterations

---
+++ Clothing alterations.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clothes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    design TEXT);
+    
+INSERT INTO clothes (type, design)
+    VALUES ("dress", "pink polka dots");
+INSERT INTO clothes (type, design)
+    VALUES ("pants", "rainbow tie-dye");
+INSERT INTO clothes (type, design)
+    VALUES ("blazer", "black sequin");
