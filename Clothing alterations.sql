3c1b1fdc6d1fc4e4e0b930c13e0fce514d231020
Konstrictor <jonkur96@gmail.com>
2019-12-23 11:47:49 +0200

    Clothing alterations 3

--- Clothing alterations.sql
+++ Clothing alterations.sql
@@ -19,3 +19,8 @@ UPDATE clothes SET price=20 WHERE id=2;
 UPDATE clothes SET price=30 WHERE id=3;
 
 SELECT * FROM clothes;
+
+INSERT INTO clothes (type, design, price)
+    VALUES ("hat", "pink polka dots", 2);
+    
+SELECT * FROM clothes;
